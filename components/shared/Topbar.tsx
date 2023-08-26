a19cff890ea19679eb0bf241b26fa2966364e830
Tim <tim@brightsidedeveloper.com>
2023-08-26 13:42:27 -0500

    Save features

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,14 +1,14 @@
-import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
-import { dark } from "@clerk/themes";
-import Image from "next/image";
-import Link from "next/link";
+import { OrganizationSwitcher, SignedIn, SignOutButton } from '@clerk/nextjs'
+import { dark } from '@clerk/themes'
+import Image from 'next/image'
+import Link from 'next/link'
 
 function Topbar() {
   return (
     <nav className='topbar'>
       <Link href='/' className='flex items-center gap-4'>
-        <Image src='/logo.svg' alt='logo' width={28} height={28} />
-        <p className='text-heading3-bold text-light-1 max-xs:hidden'>Threads</p>
+        <Image src='/logo.png' alt='logo' width={28} height={28} />
+        <p className='text-heading3-bold text-light-1 max-xs:hidden'>X2</p>
       </Link>
 
       <div className='flex items-center gap-1'>
@@ -31,13 +31,13 @@ function Topbar() {
           appearance={{
             baseTheme: dark,
             elements: {
-              organizationSwitcherTrigger: "py-2 px-4",
+              organizationSwitcherTrigger: 'py-2 px-4',
             },
           }}
         />
       </div>
     </nav>
-  );
+  )
 }
 
-export default Topbar;
+export default Topbar
