29d1300ec767efb46536fea119b49790c03b190f
Tim <timvan0118@gmail.com>
2023-08-26 17:58:14 -0500

    Optimisitic

--- components/forms/LikePost.tsx
+++ components/forms/LikePost.tsx
@@ -2,6 +2,7 @@
 import { likePost } from "@/lib/actions/thread.actions"
 import Image from "next/image"
 import { usePathname } from "next/navigation"
+import { useState } from "react"
 
 interface LikePostProps {
     threadId: string
@@ -12,15 +13,17 @@ interface LikePostProps {
 
 export default function LikePost({threadId, userId, liked, count}: LikePostProps) {
     const path = usePathname()
+    const [optimisicLike, setOptimisticLike] = useState(false)
 
     const like = async () => {
+        setOptimisticLike(true)
         await likePost(JSON.parse(threadId), JSON.parse(userId), path)
     }
 
   return (
     <button className="flex flex-col justify-center items-center " disabled={liked} onClick={like}>
         <Image
-        src={liked ? '/assets/heart-filled.svg' : '/assets/heart-gray.svg'}
+        src={liked || optimisicLike ? '/assets/heart-filled.svg' : '/assets/heart-gray.svg'}
         alt='heart'
         width={24}
         height={24}
