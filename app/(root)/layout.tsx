60d469e88cc3b76a753866b7208b1eb8aa4e57d7
Tim <tim@brightsidedeveloper.com>
2023-08-26 14:07:32 -0500

    Finishing Up

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,26 +1,26 @@
-import React from "react";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import { ClerkProvider } from "@clerk/nextjs";
-import { dark } from "@clerk/themes";
+import React from 'react'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
+import { dark } from '@clerk/themes'
 
-import "../globals.css";
-import LeftSidebar from "@/components/shared/LeftSidebar";
-import Bottombar from "@/components/shared/Bottombar";
-import RightSidebar from "@/components/shared/RightSidebar";
-import Topbar from "@/components/shared/Topbar";
+import '../globals.css'
+import LeftSidebar from '@/components/shared/LeftSidebar'
+import Bottombar from '@/components/shared/Bottombar'
+import RightSidebar from '@/components/shared/RightSidebar'
+import Topbar from '@/components/shared/Topbar'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Threads",
-  description: "A Next.js 13 Meta Threads application",
-};
+  title: 'X2 | Better than Meta',
+  description: 'A Next.js 13 Practice Project',
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <ClerkProvider
@@ -45,5 +45,5 @@ export default function RootLayout({
         </body>
       </html>
     </ClerkProvider>
-  );
+  )
 }
