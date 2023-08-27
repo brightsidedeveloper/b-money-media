490aec85969cc2b6e17b4ae0265c211e896801a2
Tim <timvan0118@gmail.com>
2023-08-27 00:04:02 -0500

    Save

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -158,7 +158,7 @@ export async function getActivity(userId: string) {
     connectToDB();
 
     // Find all threads created by the user
-    const userThreads = await Thread.find({ author: userId });
+    const userThreads = await Thread.find({ author: userId }).sort({ createdAt: "desc" });
 
     // Collect all the child thread ids (replies) from the 'children' field of each user thread
     const childThreadIds = userThreads.reduce((acc, userThread) => {
@@ -169,7 +169,7 @@ export async function getActivity(userId: string) {
     const replies = await Thread.find({
       _id: { $in: childThreadIds },
       author: { $ne: userId }, // Exclude threads authored by the same user
-    }).populate({
+    }).sort({ createdAt: "desc" }).populate({
       path: "author",
       model: User,
       select: "name image _id",
