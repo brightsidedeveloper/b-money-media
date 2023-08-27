1ee818700cf4322a08e83f23c3a483e3fe9f4b73
Tim <timvan0118@gmail.com>
2023-08-26 23:42:02 -0500

    Looks better

--- components/shared/Refresher.tsx
+++ components/shared/Refresher.tsx
@@ -1,6 +1,6 @@
 'use client'
 export default function Refresher() {
   return (
-    <button onClick={() => window.location.reload()} className='bg-primary-500 rounded-full px-3 py-1 text-small-regular text-white'>Refresh</button>
+    <button onClick={() => window.location.reload()} className='bg-primary-500 rounded-full px-3 py-0.5 text-small-regular text-white'>Refresh</button>
   )
 }
