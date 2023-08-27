be90f8f26e83826d2009a867cc0052a469eb52da
Tim <timvan0118@gmail.com>
2023-08-26 23:50:27 -0500

    auto refresh

--- components/shared/Refresher.tsx
+++ components/shared/Refresher.tsx
@@ -1,5 +1,15 @@
 'use client'
+
+import { useEffect } from "react"
+
+
 export default function Refresher() {
+useEffect(() => {
+    const i = setInterval(() => {
+            window.location.reload()
+        }, 1000 * 60)
+        return () => clearInterval(i)
+}, [])    
   return (
     <button onClick={() => window.location.reload()} className='bg-primary-500 rounded-full px-3 py-0.5 text-small-regular text-white'>Refresh</button>
   )
