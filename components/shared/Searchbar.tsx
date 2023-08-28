9eced0f9119d88e146f08658ef06173a4eb2907b
Tim <tim@brightsidedeveloper.com>
2023-08-28 18:53:52 -0500

    search bar better

--- components/shared/Searchbar.tsx
+++ components/shared/Searchbar.tsx
@@ -1,31 +1,31 @@
-"use client";
+'use client'
 
-import Image from "next/image";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
-import { Input } from "../ui/input";
+import { Input } from '../ui/input'
 
 interface Props {
-  routeType: string;
+  routeType: string
 }
 
 function Searchbar({ routeType }: Props) {
-  const router = useRouter();
-  const [search, setSearch] = useState("");
+  const router = useRouter()
+  const [search, setSearch] = useState('')
 
   // query after 0.3s of no input
   useEffect(() => {
     const delayDebounceFn = setTimeout(() => {
       if (search) {
-        router.push(`/${routeType}?q=` + search);
+        router.push(`/${routeType}?q=` + search)
       } else {
-        router.push(`/${routeType}`);
+        router.push(`/${routeType}`)
       }
-    }, 300);
+    }, 300)
 
-    return () => clearTimeout(delayDebounceFn);
-  }, [search, routeType]);
+    return () => clearTimeout(delayDebounceFn)
+  }, [search, routeType])
 
   return (
     <div className='searchbar'>
@@ -39,14 +39,14 @@ function Searchbar({ routeType }: Props) {
       <Input
         id='text'
         value={search}
-        onChange={(e) => setSearch(e.target.value)}
+        onChange={e => setSearch(e.target.value)}
         placeholder={`${
-          routeType !== "/search" ? "Search communities" : "Search creators"
+          routeType !== 'search' ? 'Search communities' : 'Search creators'
         }`}
         className='no-focus searchbar_input'
       />
     </div>
-  );
+  )
 }
 
-export default Searchbar;
+export default Searchbar
