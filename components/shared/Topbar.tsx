2c878f930ebee2ff00fdfef0a3498e1776c7a1c2
Tim <tim@brightsidedeveloper.com>
2023-09-03 07:45:19 -0500

    massive updates

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -6,8 +6,7 @@ function Topbar() {
   return (
     <nav className='topbar'>
       <Link href='/' className='flex items-center gap-4'>
-        <Image src='/logo.png' alt='logo' width={28} height={28} />
-        <p className='text-heading3-bold text-light-1 max-xs:hidden'>X2</p>
+        <Image src='/x2-logo.png' alt='logo' width={40} height={40} />
       </Link>
 
       <div className='flex items-center gap-1'>
