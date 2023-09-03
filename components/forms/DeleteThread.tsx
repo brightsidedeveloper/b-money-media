a7072612751c6807d542aa6fbf026446298bf9cf
Tim <tim@brightsidedeveloper.com>
2023-09-03 07:17:33 -0500

    Better alignment

--- components/forms/DeleteThread.tsx
+++ components/forms/DeleteThread.tsx
@@ -1,16 +1,16 @@
-"use client";
+'use client'
 
-import Image from "next/image";
-import { usePathname, useRouter } from "next/navigation";
+import Image from 'next/image'
+import { usePathname, useRouter } from 'next/navigation'
 
-import { deleteThread } from "@/lib/actions/thread.actions";
+import { deleteThread } from '@/lib/actions/thread.actions'
 
 interface Props {
-  threadId: string;
-  currentUserId: string;
-  authorId: string;
-  parentId: string | null;
-  isComment?: boolean;
+  threadId: string
+  currentUserId: string
+  authorId: string
+  parentId: string | null
+  isComment?: boolean
 }
 
 function DeleteThread({
@@ -20,26 +20,30 @@ function DeleteThread({
   parentId,
   isComment,
 }: Props) {
-  const pathname = usePathname();
-  const router = useRouter();
+  const pathname = usePathname()
+  const router = useRouter()
 
-  if (currentUserId !== authorId || pathname === "/") return null;
+  if (currentUserId !== authorId || pathname === '/') return null
 
   return (
-    <Image
-      src='/assets/delete.svg'
-      alt='delte'
-      width={18}
-      height={18}
-      className='cursor-pointer object-contain'
+    <button
       onClick={async () => {
-        await deleteThread(JSON.parse(threadId), pathname);
+        await deleteThread(JSON.parse(threadId), pathname)
         if (!parentId || !isComment) {
-          router.push("/");
+          router.push('/')
         }
       }}
-    />
-  );
+      className='ml-4'
+    >
+      <Image
+        src='/assets/delete.svg'
+        alt='delte'
+        width={18}
+        height={18}
+        className='cursor-pointer object-contain'
+      />
+    </button>
+  )
 }
 
-export default DeleteThread;
+export default DeleteThread
