e6cb22454a23c67c73ccd119f957c478ad6cab29
Tim <tim@brightsidedeveloper.com>
2023-09-04 14:38:25 -0500

    Better

--- lib/validations/thread.ts
+++ lib/validations/thread.ts
@@ -1,7 +1,7 @@
 import * as z from 'zod'
 
 export const ThreadValidation = z.object({
-  thread: z.string().nonempty().min(3, { message: 'Minimum 3 characters.' }),
+  thread: z.string().nonempty().min(1, { message: 'Minimum 3 characters.' }),
   accountId: z.string(),
 })
 
