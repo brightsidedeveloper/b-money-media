31ceea3ef1319d7035a38a0a200f1864f57ab620
Tim <timvan0118@gmail.com>
2023-09-13 23:06:15 -0500

    Better now!

--- public/x2-sw.js
+++ public/x2-sw.js
@@ -22,5 +22,18 @@ self.addEventListener("notificationclick", function (event) {
   const urlToOpen = event.notification.data.url
 
   // Open the URL in a new window/tab
-  event.waitUntil(clients.openWindow(urlToOpen))
+  event.waitUntil(clients.openWindow())
+
+  // Open the PWA
+  event.waitUntil(
+    clients.matchAll({ type: "window" }).then(clientsList => {
+      for (const client of clientsList) {
+        if ("navigate" in client) {
+          return client.navigate(urlToOpen) // Replace with the appropriate URL for your PWA
+        }
+      }
+      // If no open window is found, open the PWA in a new window
+      return clients.openWindow(urlToOpen) // Replace with the appropriate URL for your PWA
+    })
+  )
 })
